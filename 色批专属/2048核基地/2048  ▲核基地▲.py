f118a0d25432b79e3273f25c3472883d393f9177
石磊 <67324938+SHILEI-SPACE@users.noreply.github.com>
2022-03-11 21:09:15 +0800

    Update 2048  ▲核基地▲.py

--- 色批专属/2048核基地/2048  ▲核基地▲.py
+++ 色批专属/2048核基地/2048  ▲核基地▲.py
@@ -40,9 +40,9 @@ if __name__ == '__main__':
     # Aria2参数设置
     aria2 = aria2p.API(
         aria2p.Client(
-            host="",
+            host="http://127.0.0.1",
             port=6800,
-            secret="YuSheng"
+            secret=""
         )
     )
     #  伪装头列表
